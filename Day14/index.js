4db1aed143ea5d93747c53558cf09d8234ca9b44
Harshi227 <harshi0756@gmail.com>
2026-09-25 22:29:39 +0530

    Day14

---
+++ Day14/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+
+const app = express();
+const PORT = 3005;
+
+app.use(morgan('dev'));
+
+app.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // limit each IP to 5 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use('/', limiter); // Apply rate limiting to all requests under /api/
+
+app.get('/', (req, res) => {
+  res.send('Hello 3rd Party middleware!');
+});
+
+app.get('/api/data', (req, res) => {
+  res.json({ msg: 'Sample API response' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
